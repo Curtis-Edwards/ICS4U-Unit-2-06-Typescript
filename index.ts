a25ba79baade50d8c289883866e3c6f698a1a589
Curtis-Edwards <99354904+Curtis-Edwards@users.noreply.github.com>
2024-04-13 10:33:29 -0400

    Update index.ts

--- index.ts
+++ index.ts
@@ -6,13 +6,13 @@
 * @since   2024-04-13
 */
 
-import Airplane from "./Airplane"
-import Jet from "./Jet"
+import Airplane from './Airplane'
+//import Jet from './Jet'
 
 const biplane = new Airplane()
 biplane.setSpeed(212)
 console.log(biplane.getSpeed())
-const boeing = new Jet()
+/*const boeing = new Jet()
 boeing.setSpeed(422)
 console.log(boeing.getSpeed())
 let counter = 0
