40980c68e337b11fdb438cec92c4b304745b5f93
Curtis-Edwards <99354904+Curtis-Edwards@users.noreply.github.com>
2024-04-13 09:39:06 -0400

    Create Airplane.ts

---
+++ Airplane.ts
@@ -0,0 +1,32 @@
+/*
+ * Airplane class.
+ *
+ * @author  Curtis Edwards
+ * @version 1.0
+ * @since   2024-04-13
+ */
+
+export default class Airplane {
+    private speed: number
+  
+    /*
+    * constructor
+    */
+    constructor() {
+      this.speed = 0
+    }
+  
+    /*
+    * This method sets the speed
+    */
+    public set setSpeed(newSpeed: number) {
+        this.speed = newSpeed
+    }
+
+    /*
+    * This method gets the speed
+    */
+    public get getSpeed(): number {
+      return this.speed
+    }
+}
