bd73d41d810c3fb69c46f0044e1097fb45634d40
Curtis-Edwards <99354904+Curtis-Edwards@users.noreply.github.com>
2024-04-13 10:24:06 -0400

    Update Airplane.ts

--- Airplane.ts
+++ Airplane.ts
@@ -6,27 +6,27 @@
  * @since   2024-04-13
  */
 
-export class Airplane {
-    private _speed: number
+export default class Airplane {
+    private speed: number
   
     /*
     * constructor
     */
     constructor() {
-      this._speed = 0
+      this.speed = 0
     }
   
     /*
     * This method sets the speed
     */
     public set setSpeed(newSpeed: number) {
-        this._speed = newSpeed
+        this.speed = newSpeed
     }
 
     /*
     * This method gets the speed
     */
     public get getSpeed(): number {
-      return this._speed
+      return this.speed
     }
 }
