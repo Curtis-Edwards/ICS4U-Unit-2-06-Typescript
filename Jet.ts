c3f85a0dcf6bcd1df82b139363c4cda6c0e7b81a
Curtis-Edwards <99354904+Curtis-Edwards@users.noreply.github.com>
2024-04-13 09:58:23 -0400

    Create Jet.ts

---
+++ Jet.ts
@@ -0,0 +1,38 @@
+/*
+* The Jet class.
+*
+* @author  Curtis Edwards
+* @version 1.0
+* @since   2024-04-13
+*/
+
+import { Airplane } from "./Airplane"
+
+/**
+* This is the jet class.
+*/
+export class Jet extends Airplane {
+    /** Jet speed change multiplier. */
+    private MULTIPLIER: number = 2
+
+    /**
+     * Constructs a new Jet object.
+     */
+    public Jet() {
+        super()
+    }
+
+    /**
+     * Sets the speed of the jet.
+     */
+    public setSpeed(speed: number) {
+        super.setSpeed(speed * this.MULTIPLIER)
+    }
+
+    /**
+     * Accelerates the jet.
+     */
+    public accelerate() {
+        super.setSpeed(super.getSpeed() * 2)
+    }
+}
